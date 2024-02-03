2f39a8b2e98e7f55a332c062db16c78f6a326481
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-02-04 01:23:44 +0530

    added cart ui

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  greeting = '';
+
   handleChange(event:any) {
   
   }
-  constructor() {}
+  constructor() {
+    const now = new Date();
+    const hour = now.getHours();
+    if (hour >= 5 && hour < 12) {
+      this.greeting = 'Good morning';
+    } else if (hour >= 12 && hour < 18) {
+      this.greeting = 'Good afternoon';
+    } else {
+      this.greeting = 'Good night';
+    }
+  }
   pinFormatter(value: number) {
     return `$${value}`;
   }
