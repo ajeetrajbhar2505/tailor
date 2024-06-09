a872dc5e31335ddec9270d0e884da3e52e2d98d5
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-06-09 15:30:45 +0530

    implemented  showloadRewardedInterstialAds

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WebService } from '../web.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-files',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './files.component.scss'
 })
 export class FilesComponent {
+
+  constructor(private _https:WebService,private router:Router){}
+
+
+  showloadRewardedInterstialAds(){    
+    this._https.loadRewardedInterstialAd()
+    .then((result: boolean) => {
+      this._https.showloadRewardedInterstialAds()
+        .then((result: boolean) => {
+          console.log({result});
+          this.router.navigate(['/watch'])
+        })
+    })
+
+  }
 
 }
