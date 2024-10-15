2a635e713b2667805adfd580ec362a06dd6c1953
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-10-15 21:45:40 +0530

    monir update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProfileInfoComponent } from './profile-info/profile-info.component';
-import { VideoPlayerComponent } from './video-player/video-player.component';
-import { CourseComponent } from './course/course.component';
-import { FilesComponent } from './files/files.component';
-import { MembershipPlanComponent } from './membership-plan/membership-plan.component';
-import { FlashMessageComponent } from './flash-message/flash-message.component';
-import { ComingSoonComponent } from './coming-soon/coming-soon.component';
-import { FillupdetailsComponent } from './fillupdetails/fillupdetails.component';
-import { CitiesComponent } from './cities/cities.component';
 
 const routes: Routes = [
   {path : '',redirectTo : '',pathMatch : 'full'},
   {path : '',component : HomeComponent},
-  {path : 'membership-plan',component : MembershipPlanComponent},
-  {path : 'watch',component : VideoPlayerComponent},
-  {path : 'cities',component : CitiesComponent},
-  {path : 'coming-soon',component : ComingSoonComponent},
-  {path : 'fill-up',component : FillupdetailsComponent},
 ]
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,37 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { ProfileInfoComponent } from './profile-info/profile-info.component';
-import { VideoPlayerComponent } from './video-player/video-player.component';
-import { CourseComponent } from './course/course.component';
-import { FilesComponent } from './files/files.component';
-import { MembershipPlanComponent } from './membership-plan/membership-plan.component';
-import { FlashMessageComponent } from './flash-message/flash-message.component';
 import { WebService } from './web.service';
-import { ComingSoonComponent } from './coming-soon/coming-soon.component';
-import { FillupdetailsComponent } from './fillupdetails/fillupdetails.component';
-import { BannerComponent } from './banner/banner.component';
-import { CitiesComponent } from './cities/cities.component';
 import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AppComponent,
     HomeComponent,
-    ProfileInfoComponent,
-    VideoPlayerComponent,
-    CourseComponent,
-    FilesComponent,
-    MembershipPlanComponent,
-    ComingSoonComponent,
-    FillupdetailsComponent,
-    FlashMessageComponent,
-    BannerComponent,
-    CitiesComponent
   ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },WebService],
   bootstrap: [AppComponent],
-  exports :[FillupdetailsComponent]
+  exports :[]
 })
 export class AppModule {}
